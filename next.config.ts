2e91525261ab8b8861f00e564156243cf5666f41
Havryliuk Andrii <a.havryliuk@dao-group.org>
2025-07-11 14:34:20 +0300

    remove custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/birth-day-quest',
+  assetPrefix: '/birth-day-quest/',
 };
 
 export default nextConfig;
