2d5773195c156271d607ded60c2625b9f1e00ec3
Havryliuk Andrii <a.havryliuk@dao-group.org>
2025-07-12 06:05:28 +0300

    fix alls

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/birth-day-quest',
+  assetPrefix: '/birth-day-quest/',
 };
 
 export default nextConfig;
